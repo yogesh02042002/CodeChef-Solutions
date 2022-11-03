5c7843cbaf54018b0bbd6403b97bccd232f4042b
Yogesh B <102524901+yogesh02042002@users.noreply.github.com>
2022-11-03 22:14:42 +0530

    Create Problems in your to-do list.py

---
+++ Problems in your to-do list.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    n=int(input())
+    count = 0
+    a=list(map(int,input().split()))
+    for i in range(n):
+        if a[i] >=1000:
+            count+=1
+    print(count)
