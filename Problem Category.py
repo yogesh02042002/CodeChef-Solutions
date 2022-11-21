0968870ddd49fc242d6cdc56278053946e52b092
Yogesh B <102524901+yogesh02042002@users.noreply.github.com>
2022-11-21 09:15:18 +0530

    Create Problem Category.py

---
+++ Problem Category.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    x=int(input())
+    if x<100:
+        print("Easy")
+    elif x<200:
+        print("Medium")
+    elif x<=300:
+        print("Hard")
+    
