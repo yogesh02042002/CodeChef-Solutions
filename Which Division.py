b2a018d737e993e7dab71777343a1f988234a637
Yogesh B <102524901+yogesh02042002@users.noreply.github.com>
2022-11-22 20:52:10 +0530

    Create Which Division.py

---
+++ Which Division.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    r=int(input())
+    if r>=2000:
+        print("1")
+    elif r<1600:
+        print("3")
+    elif r>=1600 and r<2000:
+        print("2")
+    
