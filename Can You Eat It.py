b2081acaae55ff44c852147265e831f8a423aa3a
Yogesh B <102524901+yogesh02042002@users.noreply.github.com>
2022-12-13 21:01:37 +0530

    Create Can You Eat It.py

---
+++ Can You Eat It.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n,k=map(int,input().split())
+    if n==0 or k==0:
+        print("0")
+    elif n%k==0:
+        print(n//k)
+    else:
+        print("-1")
